4004cad4abf337d6b035d9a9f48e5ee6658e099f
Islamelbialy <islam.mohamed@mans.edu.eg>
2024-11-27 20:21:09 +0200

    lesson 2

--- company/models.py
+++ company/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Branches(models.Model):
+    name = models.CharField(null=False,unique=True,max_length=50)
+    address = models.CharField(max_length=100,blank=True,null=True)
+    phone = models.CharField(max_length=50, blank=True, null=True) 
+    email = models.EmailField(blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Departments(models.Model):
+    name = models.CharField(null=False,unique=True,max_length=50)
+    description = models.TextField(null=False,max_length=255)
+    phone = models.CharField(max_length=50, blank=True, null=True) 
+    email = models.EmailField(blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    branche = models.ForeignKey('Branches',related_name="Branche_Department",on_delete= models.CASCADE)
--- hrsystem/settings.py
+++ hrsystem/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hrsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AC_online_HR_system',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # Set to your database server
+        'PORT': '5432',       # Default port for PostgreSQL
     }
 }
 
